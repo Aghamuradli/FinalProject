5efdd5951990e5f6312feaa8f3dad7367cacf424
BabekAga <82724876+BabekAga@users.noreply.github.com>
2022-10-23 17:35:22 +0400

    Create DTOs and refactoring

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,22 @@ namespace DataAccess.Concrete.EntityFramework
     //Using(IDispossiple pattern iplementtation of C#) - garbagecollection'a blok bitdikden sora meni sil deyir
     public class EfProductDal : EfEntityRepositoryBase<Product, NorthWindContext>, IProductDal
     {
-
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthWindContext context = new NorthWindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
